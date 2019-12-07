3875fe7ac0018398f993453c4bc38c6ab05975b9
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2019-12-07 16:50:17 +0800

    Added button functions and food group calculation

--- src/FoodSelectionFrame.java
+++ src/FoodSelectionFrame.java
@@ -3,8 +3,11 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -12,6 +15,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -49,7 +53,7 @@ public class FoodSelectionFrame extends JFrame {
 		setContentPane(contentPane);
 
 		// Resize background image to fit window
-		ImageIcon welcomeMascot = new ImageIcon("/Users/iris/Desktop/chewpaca2.jpg"); // Only use absolute path for
+		ImageIcon welcomeMascot = new ImageIcon("chewpaca2.jpg"); // Only use absolute path for
 																						// testing purpose
 		Image originalImage = welcomeMascot.getImage();
 		Image resizedImage = originalImage.getScaledInstance(800, 600, java.awt.Image.SCALE_SMOOTH); // Resize image to
@@ -105,11 +109,31 @@ public class FoodSelectionFrame extends JFrame {
 		btnFindOut.setBounds(506, 384, 145, 62);
 		btnFindOut.setVisible(false);
 		this.getContentPane().add(btnFindOut);
+		/**
+		 * @author Xiaolu
+		 * Add button functions to save user selection and move to next frame.
+		 */
+		btnFindOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String food = cbFood.getSelectedItem().toString();
+				double w = Double.parseDouble(weight.getText());
+				UserSelection u = new UserSelection(); //need to create the user and user selection object in the mainwindow 
+				u.setFood(food);
+				if(cbWeightUnit.getSelectedItem().toString().equals("kg")) {
+					u.setWeight(w);
+				}
+				else {
+					u.setWeight(w / 1000);
+				}
+				ResultsFrame rf = new ResultsFrame();
+				rf.setVisible(true);
+				setVisible(false);
+		}});
 
 		// FOOD CATEGORY SELECTION
 		JLabel lblCategory = new JLabel("What category does your food fall under?");
 		lblCategory.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCategory.setFont(new Font("Apple LiGothic", Font.PLAIN, 20));
+		lblCategory.setFont(new Font("Apple LiGothic", Font.PLAIN, 16));
 		lblCategory.setForeground(Color.WHITE);
 		lblCategory.setBounds(414, 57, 330, 48);
 		this.getContentPane().add(lblCategory);
@@ -119,17 +143,13 @@ public class FoodSelectionFrame extends JFrame {
 		cbFoodCategories.setBounds(460, 117, 240, 27);
 		cbFoodCategories.addItem("Choose one...");
 
-		// NOTE: Replace this portion with function that provides ArrayList of food
-		// categories
-		cbFoodCategories.addItem("Cereals");
-		cbFoodCategories.addItem("Dairy");
-		cbFoodCategories.addItem("Desserts");
-		cbFoodCategories.addItem("Drinks");
-		cbFoodCategories.addItem("Fish");
-		cbFoodCategories.addItem("Fruits");
-		cbFoodCategories.addItem("Meats");
-		cbFoodCategories.addItem("Nuts");
-		cbFoodCategories.addItem("Vegetables");
+		/**
+		 * @author Xiaolu 
+		 * Read food group from the back end.
+		 */
+		for(String s : new Calculator().generalGroupSelect(new FoodDataReader("FoodGHG.csv"))) {
+			cbFoodCategories.addItem(s);
+		}
 
 		this.getContentPane().add(cbFoodCategories);
 
@@ -138,58 +158,20 @@ public class FoodSelectionFrame extends JFrame {
 				if (!cbFoodCategories.getSelectedItem().toString().equals("Choose one...")) {
 					lblFood.setVisible(true);
 					cbFood.removeAllItems();
-
-					// NOTE: Hardcoded for now. Change code to access ArrayLists from Hashmap later.
-					if (cbFoodCategories.getSelectedItem().equals("Cereals")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Barley");
-						cbFood.addItem("Maize/corn");
-						cbFood.addItem("Oats");
-					} else if (cbFoodCategories.getSelectedItem().equals("Dairy")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Butter");
-						cbFood.addItem("Cheese");
-						cbFood.addItem("Eggs");
-					} else if (cbFoodCategories.getSelectedItem().equals("Desserts")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Chocolate");
-					} else if (cbFoodCategories.getSelectedItem().equals("Drinks")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Coffee");
-						cbFood.addItem("Milk");
-						cbFood.addItem("Wine");
-					} else if (cbFoodCategories.getSelectedItem().equals("Fish")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Tuna");
-						cbFood.addItem("Lobster");
-						cbFood.addItem("Salmon");
-					} else if (cbFoodCategories.getSelectedItem().equals("Dairy")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Butter");
-						cbFood.addItem("Cheese");
-						cbFood.addItem("Eggs");
-					} else if (cbFoodCategories.getSelectedItem().equals("Fruits")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Apple");
-						cbFood.addItem("Orange");
-						cbFood.addItem("Grapes");
-					} else if (cbFoodCategories.getSelectedItem().equals("Meats")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Chicken");
-						cbFood.addItem("Pork");
-						cbFood.addItem("Beef");
-					} else if (cbFoodCategories.getSelectedItem().equals("Nuts")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Peanut");
-						cbFood.addItem("Almond");
-						cbFood.addItem("Cashew");
-					} else if (cbFoodCategories.getSelectedItem().equals("Vegetables")) {
-						cbFood.addItem("Choose one...");
-						cbFood.addItem("Lettuce");
-						cbFood.addItem("Carrot");
-						cbFood.addItem("Spinach");
+					/**
+					 * @author Xiaolu
+					 * read food contributes from backend. Have deleted the Hardcodes.
+					 */
+					Calculator c = new Calculator();
+					ArrayList<String> s = c.generalGroupSelect(new FoodDataReader("FoodGHG.csv"));
+					for(int i = 0; i < s.size(); i++) {
+						if (cbFoodCategories.getSelectedItem().equals(s.get(i))) {
+							ArrayList<String> a = c.foodGroupSelect(s.get(i));
+							for(int j = 0; j < a.size(); j++) {
+								cbFood.addItem(a.get(j));
+							}
+						}
 					}
-
 					cbFood.setVisible(true);
 					weight.setVisible(true);
 					lblWeight.setVisible(true);
