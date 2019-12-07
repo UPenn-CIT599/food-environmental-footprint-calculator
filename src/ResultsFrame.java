5da4785761415bf4310b412ba26e08664159e1af
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2019-12-07 17:12:11 +0800

    updated with button events and functions

--- src/ResultsFrame.java
+++ src/ResultsFrame.java
@@ -3,6 +3,8 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -43,13 +45,19 @@ public class ResultsFrame extends JFrame {
 		setContentPane(contentPane);
 
 		// Hardcoded now, but to instantiate the variables with calculator function later
-		double weightKG = 0.5;
-		String selectedFood = "Butter";
-		double equivalentGHG = 0.4;
-
-		AnnualGHGCalculator calculator = new AnnualGHGCalculator(equivalentGHG);
+		/**
+		 * @author Xiaolu
+		 * see the functions below
+		 */
+		UserSelection us = new UserSelection();//this should be the same userSelection object which has created last frame
+		                                         //Else it will throw null pointer exception.
+		Calculator c = new Calculator();
+		double weightKG = us.getWeight();
+		String selectedFood = us.getFood();
+		double equivalentGHG = c.getFoodGHGEmission(selectedFood, weightKG);
 
 		// Overall description of user's GHG(eq) emissions
+		double annualGHG = c.getAnnualGHG(equivalentGHG);
 		String resultsString = "<html>If you eat "+ weightKG +" kg of " + selectedFood.toLowerCase() + " everyday for one year,<br> you will contribute to <b>" + equivalentGHG + "</b> kg of CO2(eq), <br>which is equivalent to...</html>";
 		JLabel results = new JLabel(resultsString);
 		results.setBackground(Color.WHITE);
@@ -59,7 +67,7 @@ public class ResultsFrame extends JFrame {
 		this.getContentPane().add(results);
 
 		// Displays the GHG(eq) in terms of flight kilometres		
-		String flightString = "<html>... flying on a plane over <b>"+String.format("%.2f", calculator.getEquivalentFlightKM())+"</b> kilometre(s)";
+		String flightString = "<html>... flying on a plane over <b>"+String.format("%.2f", c.getEquivalentFlightKM(annualGHG))+"</b> kilometre(s)";
 		JLabel resultsFlightKM = new JLabel(flightString);
 		resultsFlightKM.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		resultsFlightKM.setBounds(182, 206, 220, 70);
@@ -67,7 +75,7 @@ public class ResultsFrame extends JFrame {
 		this.getContentPane().add(resultsFlightKM);
 
 		// Displays the GHG(eq) in terms of lightbulb days
-		String lightbulbString = "<html>... powering a typical LED lightbulb for <b>"+String.format("%.2f", calculator.getEquivalentLightbulbDays())+"</b> day(s)";
+		String lightbulbString = "<html>... powering a typical LED lightbulb for <b>"+String.format("%.2f", c.getEquivalentBulbLightDays(annualGHG))+"</b> day(s)";
 		JLabel resultsLightbulbDays = new JLabel(lightbulbString);
 		resultsLightbulbDays.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		resultsLightbulbDays.setBounds(271, 340, 220, 70);
@@ -75,7 +83,7 @@ public class ResultsFrame extends JFrame {
 		this.getContentPane().add(resultsLightbulbDays);
 
 		// Displays the GHG(eq) in terms of car kilometres		
-		String carString = "<html>... driving a passenger car for <b>"+String.format("%.2f", calculator.getEquivalentCarKM())+"</b> kilometre(s)";
+		String carString = "<html>... driving a passenger car for <b>"+String.format("%.2f", c.getCarKMEquivalent(annualGHG))+"</b> kilometre(s)";
 		JLabel resultsCarKM = new JLabel(carString);
 		resultsCarKM.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		resultsCarKM.setBounds(105, 452, 200, 70);
@@ -88,10 +96,20 @@ public class ResultsFrame extends JFrame {
 		btnExploreAltFood.setForeground(Color.BLACK);
 		btnExploreAltFood.setBounds(597, 301, 150, 70);
 		this.getContentPane().add(btnExploreAltFood);
-
+		/**
+		 * @author Xiaolu
+		 * Add listener for this button
+		 */
+		btnExploreAltFood.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AlternativeFoodFrame af = new AlternativeFoodFrame();
+				af.setVisible(true);
+				setVisible(false);
+			}
+		});
 
 		// Resize background image to fit window
-		ImageIcon welcomeMascot = new ImageIcon("/Users/iris/Desktop/chewpaca3.jpg"); // Only use absolute path for testing purpose
+		ImageIcon welcomeMascot = new ImageIcon("chewpaca3.jpg"); // Only use absolute path for testing purpose
 		Image originalImage = welcomeMascot.getImage();
 		Image resizedImage = originalImage.getScaledInstance(800, 600, java.awt.Image.SCALE_SMOOTH); // Resize image to fit welcomeRightPanel
 		this.getContentPane().setLayout(null);
@@ -104,5 +122,4 @@ public class ResultsFrame extends JFrame {
 		this.getContentPane().add(backgroundImage);
 
 	}
-
 }
